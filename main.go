b84855a4fb6f899871397b75ff04f2d4f9490cb0
Darren Burns <darrenb900@gmail.com>
2026-02-19 22:03:21 +0000

    Setting version at build time

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	t "github.com/darrenburns/terma"
 )
 
+// Set at build time by GoReleaser via ldflags.
+var (
+	version = "dev"
+	commit  = "none"
+)
+
 func main() {
 	var staged bool
+	var showVersion bool
 	flag.BoolVar(&staged, "staged", false, "start focused on staged changes")
+	flag.BoolVar(&showVersion, "version", false, "print version and exit")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("dv %s (%s)\n", version, commit)
+		os.Exit(0)
+	}
 
 	cwd, err := os.Getwd()
 	if err != nil {
